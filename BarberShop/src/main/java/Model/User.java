3750f44c3eb91803c1a7dfa976c860fa5fdd696a
larissaborsari <larissaborsari@hotmail.com>
2023-02-19 11:40:28 -0300

    feat user constructor getters and setters

--- BarberShop/src/main/java/Model/User.java
+++ BarberShop/src/main/java/Model/User.java
@@ -22,5 +22,99 @@ public class User {
     private String email;
     private String idnumber;
     private String address;
+
+    public User(int id, String name, String password, String accessLevel, char gender, Date birthDate, String phone, String email, String idnumber, String address) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.accessLevel = accessLevel;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.phone = phone;
+        this.email = email;
+        this.idnumber = idnumber;
+        this.address = address;
+    }
+
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(String accessLevel) {
+        this.accessLevel = accessLevel;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIdnumber() {
+        return idnumber;
+    }
+
+    public void setIdnumber(String idnumber) {
+        this.idnumber = idnumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+    
     
 }
